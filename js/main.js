77230d80446dd8f557295f396237bfd3219533f7
Jokerazz <youiever.ao@gmail.com>
2020-08-21 20:41:41 +0600

    added media

--- js/main.js
+++ js/main.js
@@ -178,7 +178,32 @@ $(function () {
         $('.modal-popup').fadeOut();
     });
 
+    $('.num-in span').click(function () {
+        var $input = $(this).parents('.num-block').find('input.in-num');
+      if($(this).hasClass('minus')) {
+        var count = parseFloat($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        if (count < 2) {
+          $(this).addClass('dis');
+        }
+        else {
+          $(this).removeClass('dis');
+        }
+        $input.val(count);
+      }
+      else {
+        var count = parseFloat($input.val()) + 1
+        $input.val(count);
+        if (count > 1) {
+          $(this).parents('.num-block').find(('.minus')).removeClass('dis');
+        }
+      }
+      
+      $input.change();
+      return false;
+    });
     
+
     
     $(".js-range-slider").ionRangeSlider({
         type: "double",
