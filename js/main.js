c446c8ac2a1197c38466b1a769b642c277602e94
Jokerazz <youiever.ao@gmail.com>
2020-08-29 17:11:38 +0600

    fixed basket counter

--- js/main.js
+++ js/main.js
@@ -178,30 +178,30 @@ $(function () {
         $('.modal-popup').fadeOut();
     });
 
-    $('.num-in span').click(function () {
-        var $input = $(this).parents('.num-block').find('input.in-num');
-      if($(this).hasClass('minus')) {
-        var count = parseFloat($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        if (count < 2) {
-          $(this).addClass('dis');
-        }
-        else {
-          $(this).removeClass('dis');
-        }
-        $input.val(count);
-      }
-      else {
-        var count = parseFloat($input.val()) + 1
-        $input.val(count);
-        if (count > 1) {
-          $(this).parents('.num-block').find(('.minus')).removeClass('dis');
-        }
-      }
+    // $('.num-in span').click(function () {
+    //     var $input = $(this).parents('.num-block').find('input.in-num');
+    //   if($(this).hasClass('minus')) {
+    //     var count = parseFloat($input.val()) - 1;
+    //     count = count < 1 ? 1 : count;
+    //     if (count < 2) {
+    //       $(this).addClass('dis');
+    //     }
+    //     else {
+    //       $(this).removeClass('dis');
+    //     }
+    //     $input.val(count);
+    //   }
+    //   else {
+    //     var count = parseFloat($input.val()) + 1
+    //     $input.val(count);
+    //     if (count > 1) {
+    //       $(this).parents('.num-block').find(('.minus')).removeClass('dis');
+    //     }
+    //   }
       
-      $input.change();
-      return false;
-    });
+    //   $input.change();
+    //   return false;
+    // });
 
     $(".num-in span").click(function () {
       var $input = $(this).parents(".num-block").find("input.in-num");
